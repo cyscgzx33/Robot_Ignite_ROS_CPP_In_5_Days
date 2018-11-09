d697d86271bdb0298a4862a52afec7af9d165a52
Ethan Chen <chenyus@umich.edu>
2018-11-09 13:27:21 +0000

    changed the position of ros::Rate, launched node successfully but no signal published

--- topic_mini_proj/src/motion_control_node.cpp
+++ topic_mini_proj/src/motion_control_node.cpp
@@ -3,11 +3,14 @@
 int main(int argc, char** argv) {
     ros::init(argc, argv, "motion_control");
     
-    ros::Rate loop_rate(2);
-    
     ros::NodeHandle nh("~");
     
     topic_mini_proj::motionControl mc_nh(nh);
+    
+    
+    /* one must call ros::Rate AFTER ros::NodeHandle */
+    ros::Rate loop_rate(2);
+    
     
     ros::spin();
     
