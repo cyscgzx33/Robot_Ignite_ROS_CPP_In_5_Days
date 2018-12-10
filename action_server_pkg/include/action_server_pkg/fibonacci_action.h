24a670bb56bd29d77d63344b1470844ea39c7750
Ethan Chen <chenyus@umich.edu>
2018-12-10 03:37:52 +0000

    added some comments to assist understanding

--- action_server_pkg/include/action_server_pkg/fibonacci_action.h
+++ action_server_pkg/include/action_server_pkg/fibonacci_action.h
@@ -1,9 +1,17 @@
 #include <ros/ros.h>
 #include <actionlib/server/simple_action_server.h>
+
+// in this case, the action server is using an action message definition called Fabonacci.action
+// that message has been created by ROS and written into its actionlib_tutorials package
 #include <actionlib_tutorials/FibonacciAction.h>
+
 
 class FibonacciAction
 {
+    // explanation of the protected variables:
+    // the node handle is constructed and passed into the action server during construction of the action
+    // the action server is constructed in the constructor of the action
+    // the feedback and result messages are created for publishing in the action
     protected:
         ros::NodeHandle nh_;
         
@@ -16,6 +24,7 @@ class FibonacciAction
         actionlib_tutorials::FibonacciResult result_;
         
     public:
+        // in the class constructor, an action server is created
         FibonacciAction(std::string name);
         ~FibonacciAction();
         
--- action_server_pkg/src/fibonacci_action.cpp
+++ action_server_pkg/src/fibonacci_action.cpp
@@ -4,6 +4,8 @@
 #include <action_server_pkg/fibonacci_action.h>
 
 FibonacciAction::FibonacciAction(std::string name):
+    // please pay attention to "_1", it is a special place holder of the bind() function
+    // this is a boost shared pointer, given by appending "ConstPtr" to the end of the goal message type
     as_(nh_, name, boost::bind(&FibonacciAction::executeCB, this, _1), false),
     action_name_(name)
 {
@@ -27,6 +29,11 @@ void FibonacciAction::executeCB(const actionlib_tutorials::FibonacciGoalConstPtr
     ROS_INFO("%s: Executing, creating fibonacci sequence of oder %i, %i", action_name_.c_str(), goal->order, feedback_.sequence[0], feedback_.sequence[1]);
     
     // start executing the action
+    // an important component of an action server is the ability to allow an action client to request,
+    // that the current goal execution be cancelled
+    // i.e., when a client requests that the current goal to be preempted, 
+    // the action server should cancel the goal, perform necessary clean-up and call the function setPreempted(),
+    // which indicates that the action has been preempted by user's request
     for (int i = 1; i <= goal->order; i++) {
         
         // check the preempt has not been requested by the client
@@ -41,14 +48,21 @@ void FibonacciAction::executeCB(const actionlib_tutorials::FibonacciGoalConstPtr
         
         feedback_.sequence.push_back(feedback_.sequence[i] + feedback_.sequence[i-1]);
         
+        
+        // here the Fibonacci sequence is put into the feedback variable and then published on the feedback channel provided by action server
+        // then the action continues on looping and publishing feedback
         // publish the feedback
         as_.publishFeedback(feedback_);
         
+        // setting the rate at which the action server checks for preemption requests is left to the implementor of the server
         // the sleep is not necessary, the sequence is computed at 1 Hz for demonstration purpose
         rt_.sleep();
         
     }
     
+    
+    // once the action has finished computing the Fibonacci sequence the action notifies the action client,
+    // that the action completed by setting succeeded
     if (success) {
         result_.sequence = feedback_.sequence;
         ROS_INFO("%s: Succeeded", action_name_.c_str());
